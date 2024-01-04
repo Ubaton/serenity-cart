e7a8cbbc7215d9dd5ee9e306fe7f79630c337b79
Ubaton <raymondNG729@gmail.com>
2024-01-04 13:52:20 +0200

    v0.0.3 created a FooterComponent and new styles.

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react'
 import { Header } from '../../../../payload/payload-types'
 import { Gutter } from '../../Gutter'
@@ -5,19 +7,25 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 import classes from './index.module.scss'
-import MobileNav from '../MobileNav'
 import { HeaderNav } from '../Nav'
+import { noHeaderFooterUrls } from '../../../constants'
+import { usePathname } from 'next/navigation'
 
 const HeaderComponent = ({ header }: { header: Header }) => {
+  const pathname = usePathname()
+
   return (
-    <nav className={classes.header}>
+    <nav
+      className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide]
+        .filter(Boolean)
+        .join('')}
+    >
       <Gutter className={classes.wrap}>
         <Link href="/">
           <Image src="/logo-black.svg" alt="logo" width={150} height={50} />
         </Link>
 
         <HeaderNav header={header} />
-        <MobileNav header={header} />
       </Gutter>
     </nav>
   )
--- src/app/_components/Header/MobileNav/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const MobileNav = () => {
-  return <div>HeaderMobileNav</div>
-}
-
-export default MobileNav
